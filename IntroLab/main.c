12398775ab13274c9d33b0fa7a800d65c2d911f2
mutombi <mtdrayton44@gmail.com>
2018-10-11 12:25:05 +0100

    Lab1 done and lab2 ex1 done so far. Not completed ex2

--- IntroLab/main.c
+++ IntroLab/main.c
@@ -5,16 +5,16 @@ int main(void) {
 	uint32_t ii;
 	
 	// Initialize GPIO Port for LEDs
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable GPIOD clock
-	GPIOD->MODER |= GPIO_MODER_MODER12_0; // GPIOD pin 12 output
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable GPIOD clock : could have used 0x00000008
+	GPIOD->MODER |= GPIO_MODER_MODER13_0; // GPIOD pin 13 output : could have used 0x03000000
 	
 	for(;;) {
 		
-		GPIOD->BSRR = 1<<12; 	// Turn on the green LED: shifts single bit 12 times to the left
+		GPIOD->BSRR = 1<<13; 	// Turn on the orange LED: shifts single bit 12 times to the left
 		
 		for(ii=0;ii<26000000;ii++){} // FOR loop to implement a 1 second delay using  
 		
-		GPIOD->BSRR = 1<<(12+16);	// Turn off the green LED
+		GPIOD->BSRR = 1<<(13+16);	// Turn off the orange LED
 		
 		for(ii=0;ii<26000000;ii++) {}
 		
